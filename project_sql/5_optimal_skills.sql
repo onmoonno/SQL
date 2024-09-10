b4bdae8a965b44817077ddb017042cfc43726329
Yunxia Zhang <onvanessano@gmail.com>
2024-09-10 01:39:24 -0700

    modify query 5 to more concise.

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -3,37 +3,30 @@
     Optimal: High Demand and High Paying
 */
 
-WITH skills_demand AS (
+WITH skills_analysis AS (
     SELECT 
-    skills_job_dim.skill_id,
-    skills,
-    COUNT(*) AS counts
+        skills_job_dim.skill_id,
+        skills_dim.skills,
+        COUNT(*) AS counts,
+        ROUND(AVG(salary_year_avg), 0) AS avg_salary
     FROM job_postings_fact
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-    WHERE job_title_short = 'Data Analyst' AND
-    salary_year_avg IS NOT NULL AND
-    job_work_from_home = TRUE
-    GROUP BY skills_job_dim.skill_id
-), average_salary AS (
-    SELECT 
-    skills_job_dim.skill_id,
-    ROUND(AVG(salary_year_avg), 0) AS avg_salary
-    FROM job_postings_fact
-    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-    WHERE job_title_short = 'Data Analyst' AND
-    salary_year_avg IS NOT NULL AND
-    job_work_from_home = TRUE
-    GROUP BY skills_job_dim.skill_id
+    WHERE job_title_short = 'Data Analyst' 
+      AND salary_year_avg IS NOT NULL 
+      AND job_work_from_home = TRUE
+    GROUP BY skills_job_dim.skill_id, skills_dim.skills
 )
 
 SELECT
-    skills_demand.skill_id,
-    skills_demand.skills,
+    skill_id,
+    skills,
     counts,
     avg_salary
 FROM
-    skills_demand
-INNER JOIN average_salary
-ON skills_demand.skill_id = average_salary.skill_id
+    skills_analysis
+ORDER BY
+    avg_salary DESC,
+    counts DESC
+LIMIT 25
+    
