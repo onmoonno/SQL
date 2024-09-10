4368c4174a58a1bfcf75e9c4e9710d01889215f7
Yunxia Zhang <onvanessano@gmail.com>
2024-09-10 01:38:42 -0700

    solve the optimal skills

--- project_sql/4_top_skills_based_on_salary.sql
+++ project_sql/4_top_skills_based_on_salary.sql
@@ -1,15 +1,16 @@
 /*
-3. What are the most in-demand skills for my role?
+4. What are the top skills based on salary for my role?
 */
 
 SELECT 
-    COUNT(*) AS counts,
-    skills
+    skills,
+    ROUND(AVG(salary_year_avg), 0) AS avg_salary
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE job_title_short = 'Data Analyst' AND
+salary_year_avg IS NOT NULL AND
 job_work_from_home = TRUE
 GROUP BY skills
-ORDER BY counts DESC 
-LIMIT 8
+ORDER BY avg_salary DESC 
+LIMIT 25
